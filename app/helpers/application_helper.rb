fa30e03de644aa064b63b8bb2c164cce350a3a83
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2018-09-24 21:51:53 -0300

    Aula dia 24 de setembro 2018.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def menu_link(description, url)
+    classValue = current_page?(url) ? 'active' : ''
+
+    content_tag(:li, class: classValue) do
+      link_to description, url
+    end
+  end
 end
